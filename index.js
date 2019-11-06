f09b4d25988f17678a35ce67611949834ddfb88c
JPUF <2009bennettj@gmail.com>
2019-11-06 15:28:00 +0000

    Handling environment variables in view

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 
 app.get('/', function(req, res) {
     res.render('index.ejs');
