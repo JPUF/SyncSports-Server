7a162d8c4e3081d474d8a8af0d36e8834aa7ec21
JPUF <2009bennettj@gmail.com>
2020-03-18 11:44:24 +0000

    room namespace with dummy request event

--- namespaces/roomEvents.js
+++ namespaces/roomEvents.js
@@ -4,7 +4,15 @@ const roomNamespace = io.of('/rooms');
 
 roomNamespace.on('connection', function (socket) {
     console.log("connection made to room namespace.");
-    socket.on('disconnect', function (username) {
+
+    socket.on("room_request", function() {
+        console.log("Received room request");
+        //get all rooms
+        const rooms = "Dummy Room object";
+        roomNamespace.emit("room_response", rooms)
+    });
+
+    socket.on('disconnect', function () {
         console.log("A disconnect from room namespace.")
     });
     
