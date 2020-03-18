18ae37a8bd4aae9a07b9fb75f1dffa228d4179ee
JPUF <2009bennettj@gmail.com>
2020-03-18 11:58:43 +0000

    included necessary helper function

--- namespaces/roomEvents.js
+++ namespaces/roomEvents.js
@@ -23,3 +23,20 @@ roomNamespace.on('connection', function (socket) {
     });
     
 });
+
+function removeExpiredRooms(roomArray) {    
+    for( let room in roomArray) {
+        console.log("EXPIRE CHECK: " + room + " : " + roomArray[room].last_used)
+        let isEmpty = roomArray[room].member_count == 0 ? true : false;
+        let timeSinceLastUsed = Date.now() - roomArray[room].last_used
+        let threeHours = 1000 * 60 * 60 * 3;
+
+        if(isEmpty && timeSinceLastUsed >= threeHours) {
+            console.log("expired: " + room)
+            //Delete room
+            db.ref("/rooms/"+room).remove()
+            delete roomArray[room];
+        }
+    }
+    return roomArray;
+}
